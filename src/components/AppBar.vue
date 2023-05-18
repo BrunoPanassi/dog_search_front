cf8d4c7d3cdb4eea6c48c1042ae14b6fef3c7df2
BrunoPanassi <bhenrique95@outlook.com>
2023-05-18 14:20:19 -0300

    move NavigationDrawer to AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -12,24 +12,56 @@
         <v-btn variant="text" icon="mdi-filter"></v-btn>
         <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
     </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        location="left"
+    >
+        <v-select
+        class="ml-2"
+        v-for="(item, i) in menuItens"
+        :label="item.title"
+        :items="item.data"
+        :prepend-inner-icon="item.icon"
+        flat
+        >
+        </v-select>
+        <div class="d-flex justify-center align-center">
+        <v-btn color="green">
+            Procurar
+        </v-btn>
+        </div>
+    </v-navigation-drawer>
 </template>
 
 <script lang="ts" setup>
 import {ref} from 'vue';
+
+const drawer = ref<boolean>(false);
 
 const { color, title } = defineProps({
     color: { type: String, required: true},
     title: { type: String, required: true}
 })
 
-const emit = defineEmits<{
-    (e: 'clickDrawer', drawer: boolean): boolean
-}>()
-
-const drawer = ref<boolean>(false);
-
 const handleClickDrawer = () => {
     drawer.value = !drawer.value
-    emit('clickDrawer', drawer.value)
 }
+
+const menuItens = [
+    {
+      title: 'Cidade',
+      icon: 'mdi-city',
+      data: ["Araçatuba", "Birigui"]
+    },
+    {
+      title: 'Categoria',
+      icon: 'mdi-menu',
+      data: ["Categoria 1", "Categoria 2"]
+    },
+    {
+      title: 'Sub-Categoria',
+      icon: 'mdi-microsoft-xbox-controller-menu',
+      data: ["Sub-Categoria 1", "Sub-Categoria 2"]
+    }
+  ];
 </script>
--- src/components/View.vue
+++ src/components/View.vue
@@ -1,53 +1,14 @@
 <template>
     <v-layout>
-        <AppBar color="orange" title="Test" @click-drawer="handleDrawer"/> 
-        <v-navigation-drawer
-            v-model="drawer"
-            location="left"
-        >
-          <v-select
-            class="ml-2"
-            v-for="(item, i) in menuItens"
-            :label="item.title"
-            :items="item.data"
-            :prepend-inner-icon="item.icon"
-            flat
-          >
-          </v-select>
-          <div class="d-flex justify-center align-center">
-            <v-btn color="green">
-              Procurar
-            </v-btn>
-          </div>
-        </v-navigation-drawer>
+        <AppBar :color="appBar.color" :title="appBar.title"/> 
     </v-layout>
 </template>
 
 <script lang="ts" setup>
 import AppBar from '@/components/AppBar.vue';
-import {ref} from 'vue';
 
-const drawer = ref<boolean>(false);
-
-const handleDrawer = (drawerClicked: boolean) => {
-  drawer.value = drawerClicked
+const appBar = {
+  color: "orange",
+  title: "Test"
 }
-
-const menuItens = [
-    {
-      title: 'Cidade',
-      icon: 'mdi-city',
-      data: ["Araçatuba", "Birigui"]
-    },
-    {
-      title: 'Categoria',
-      icon: 'mdi-menu',
-      data: ["Categoria 1", "Categoria 2"]
-    },
-    {
-      title: 'Sub-Categoria',
-      icon: 'mdi-microsoft-xbox-controller-menu',
-      data: ["Sub-Categoria 1", "Sub-Categoria 2"]
-    }
-  ];
 </script>

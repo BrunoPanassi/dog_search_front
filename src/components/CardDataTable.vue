30ae4490baa8a3b7ee772198bb01339c45ada5f7
BrunoPanassi <bhenrique95@outlook.com>
2023-05-29 13:39:09 -0300

    update vuetify and add v-data-table

--- src/components/CardDataTable.vue
+++ src/components/CardDataTable.vue
@@ -1,12 +1,40 @@
 <template>
     <v-card class="mx-auto my-5" max-width="344">
-        <v-card-title> {{ tableSelected }} </v-card-title>
+        <v-data-table
+            :headers="headers"
+            :items="categories"
+        >
+        <template v-slot:item.actions="{ item }">
+            <v-icon
+                size="small"
+                class="me-2"
+            >
+                mdi-pencil
+            </v-icon>
+            <v-icon
+                size="small"
+            >
+                mdi-delete
+            </v-icon>
+    </template>
+        </v-data-table>
     </v-card>
 </template>
 
 <script lang="ts" setup>
 import { useAdminTableSelectStore } from '@/store/adminTableSelect';
-import { computed } from 'vue'
+import { IdAndName } from '@/types/idAndName';
+import { ref,computed, onMounted } from 'vue'
+import CategorySerice from '@/service/CategoryService'
+
+const headers = [
+  { title: "Id", key: "id" },
+  { title: "Name", key: "name"},
+  { title: "Actions", key: "actions"}
+];
+
+
+let categories = ref<Array<IdAndName>>([]);
 
 const adminTableSelectStore = useAdminTableSelectStore()
 const tableSelected = computed(() => {
@@ -14,5 +42,18 @@ const tableSelected = computed(() => {
 })
 
 const capitalize = (text: string) => { return `${text.charAt(0).toUpperCase()}${text.slice(1)}` }
+
+const getCategories = async() => { 
+    try {
+        const { data } = await CategorySerice.getAll()
+        categories.value.push(...data)
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+onMounted(async () => {
+    await getCategories();
+})
 
 </script>

fe2b783a6f5962f30db8366c932027c220e24c96
BrunoPanassi <bhenrique95@outlook.com>
2023-06-03 12:54:09 -0300

    separate dialog to a component

--- src/components/CardDataTable.vue
+++ src/components/CardDataTable.vue
@@ -4,42 +4,26 @@
             :headers="headers"
             :items="categories"
         >
-        <template v-slot:top>
-            <v-toolbar flat>
-                <v-toolbar-title>{{ tableSelected }}</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="500px">
-                    <template v-slot:activator="{ props }">
-                        <v-btn
-                            color="primary"
-                            dark
-                            v-bind="props"
-                        >
-                            Adicionar
-                        </v-btn>
-                    </template>
-                    <v-card>
-                        <v-card-title> Dialog </v-card-title>
-                        <v-btn @click.stop="dialog = false">
-                            Close
-                        </v-btn>
-                    </v-card>
-                </v-dialog>
-            </v-toolbar>
-        </template>
-        <template v-slot:item.actions="{ item }">
-            <v-icon
-                size="small"
-                class="me-2"
-            >
-                mdi-pencil
-            </v-icon>
-            <v-icon
-                size="small"
-            >
-                mdi-delete
-            </v-icon>
-    </template>
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>{{ tableSelected }}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <Dialog></Dialog>
+                </v-toolbar>
+            </template>
+            <template v-slot:item.actions="{ item }">
+                <v-icon
+                    size="small"
+                    class="me-2"
+                >
+                    mdi-pencil
+                </v-icon>
+                <v-icon
+                    size="small"
+                >
+                    mdi-delete
+                </v-icon>
+            </template>
         </v-data-table>
     </v-card>
 </template>
@@ -49,6 +33,7 @@ import { useAdminTableSelectStore } from '@/store/adminTableSelect';
 import { IdAndName } from '@/types/idAndName';
 import { ref,computed, onMounted } from 'vue'
 import CategorySerice from '@/service/CategoryService'
+import Dialog from '@/components/Dialog.vue'
 
 const headers = [
   { title: "Id", key: "id" },
@@ -56,9 +41,7 @@ const headers = [
   { title: "Actions", key: "actions"}
 ];
 
-
 let categories = ref<Array<IdAndName>>([]);
-let dialog = ref<boolean>(false);
 
 const adminTableSelectStore = useAdminTableSelectStore()
 const tableSelected = computed(() => {
---
+++ src/components/Dialog.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-dialog v-model="dialog" max-width="500px">
+        <template v-slot:activator="{ props }">
+            <v-btn
+                color="primary"
+                dark
+                v-bind="props"
+            >
+                Adicionar
+            </v-btn>
+        </template>
+        <v-card>
+            <v-card-title> Dialog </v-card-title>
+            <v-btn @click.stop="dialog = false">
+                Close
+            </v-btn>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+let dialog = ref<boolean>(false);
+
+</script>

691b7e5ecb55f76a158842e10adb043b6f18f8d3
BrunoPanassi <bhenrique95@outlook.com>
2023-06-30 09:09:21 -0300

    create user menu

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -8,9 +8,55 @@
             {{ appBarStore.getTitle }}
         </v-app-bar-title>
         <v-spacer></v-spacer>
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn 
+                    variant="text" 
+                    icon="mdi-account"
+                    v-bind="props"
+                ></v-btn>
+            </template>
+
+            <v-card min-width="300">
+                <v-list>
+                    <v-list-item
+                        title="Usuário"
+                        subtitle="Não logado"
+                    >
+                    <template v-slot:prepend>
+                        <v-avatar>
+                            <v-icon icon="mdi-account-off"></v-icon>
+                        </v-avatar>
+                    </template>
+                    </v-list-item>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                <v-list>
+                    <v-list-item
+                        v-for="(item, index) in accountMenuItens"
+                        :key="index"
+                    >
+                    <v-list-item-title>
+                        <v-btn 
+                            class="text-none text-subtitle-1" 
+                            variant="flat"
+                        >
+                            {{ item }}
+                        </v-btn>
+                    </v-list-item-title>
+                    </v-list-item>
+                </v-list>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn>Deslogar</v-btn>
+                    <v-btn>Logar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-menu>
+        
     </v-app-bar>
 </template>
 
@@ -24,6 +70,8 @@ const appBarStore = useAppBarStore();
 const handleClickDrawer = () => {
     drawerStore.drawerClicked()
 }
+
+const accountMenuItens = ["Configurações", "Criar Conta"]
 
 
 </script>
